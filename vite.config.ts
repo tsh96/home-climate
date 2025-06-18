7fc9cc255f959a584b34e56b807bfa6e8f591ec4
Tan Sia How <tansiahow1996@gmail.com>
2025-06-18 21:00:32 +0800

    fix: set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'home-climate/',
   server: {
     headers: { 'Cross-Origin-Embedder-Policy': 'unsafe-none' }
   },
